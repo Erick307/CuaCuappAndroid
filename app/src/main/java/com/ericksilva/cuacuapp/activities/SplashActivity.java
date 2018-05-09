47fc878dd5308358a52479e85267ad80acba403b
Erick Silva <ericksilva1988@gmail.com>
2018-05-09 18:29:04 -0300

    Primer commit

---
+++ app/src/main/java/com/ericksilva/cuacuapp/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.ericksilva.cuacuapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ericksilva.cuacuapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

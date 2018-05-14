c456e8c9ddbed711865f30fc5c122593af193402
Erick Silva <ericksilva1988@gmail.com>
2018-05-14 00:35:50 -0300

    Agrego las primeras activities

--- app/src/main/java/com/ericksilva/cuacuapp/activities/SplashActivity.java
+++ app/src/main/java/com/ericksilva/cuacuapp/activities/SplashActivity.java
@@ -1,15 +1,31 @@
 package com.ericksilva.cuacuapp.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ericksilva.cuacuapp.R;
+import com.ericksilva.cuacuapp.activities.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(mRunnable, SPLASH_TIME_OUT);
     }
+
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
